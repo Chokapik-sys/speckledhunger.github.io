25ad334bc7a390c8ab95180eeaa1b931c48344a1
speckledhunger <speckledhunger@users.noreply.github.com>
2016-09-12 21:38:15 +0100

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,2 +1,14 @@
 alert("Welcome to the Games site");
 alert("Feel free to browse...");
+
+function randomIframe(obj){
+var ends = new Array();
+ends[0] = "https://scratch.mit.edu/projects/embed/89672994/";
+ends[1] = "https://scratch.mit.edu/projects/embed/115608650/";
+ends[2] = "https://scratch.mit.edu/projects/embed/112865991/";
+
+var i = Math.round(Math.random()*ends.length-1);
+
+obj.location.replace(ends[i]);
+
+}
