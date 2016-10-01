32d6c8130c6d1098ac7fb762885b482acd4e9a9e
speckledhunger <speckledhunger@users.noreply.github.com>
2016-10-01 16:22:36 +0100

    Update test.js

--- js/test.js
+++ js/test.js
@@ -12,13 +12,3 @@ var i = Math.round(Math.random()*ends.length-1);
 obj.location.replace(ends[i]);
 
 }
-function get_browser(){
-    var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
-    if(/trident/i.test(M[1])){
-        tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
-        return {name:'IE',version:(tem[1]||'')};
-        }   
-    if(M[1]==='Chrome'){
-        tem=ua.match(/\bOPR\/(\d+)/)
-        if(tem!=null)   {return {name:'Opera', version:tem[1]};}
-        }   
