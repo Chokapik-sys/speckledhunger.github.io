035f05de4eba1a171937c60397ff7fcefc0600bf
speckledhunger <speckledhunger@users.noreply.github.com>
2017-02-13 15:05:05 +0000

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,7 +1,11 @@
   var num1 = prompt("Pick a number")
+  
   var num2 = prompt("Pick another number")
+  
   var sign = prompt("Choose to multiply (*), add (+), divide (/) or minus (-)")
+  
   var product = 0
+  
   if (sign === "*"){
     product = num1 * num2
   } elif (sign === "+"){
@@ -11,4 +15,5 @@
   } elif (sign === "-"){
     product = num1 - num2
   }
+
   alert (num1 + " " + sign + " " + num2 + " " + "=" + " " + product)
