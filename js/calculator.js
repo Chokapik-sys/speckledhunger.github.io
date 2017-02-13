1125a2f83ca3e63f149a527d7186815ffaa4aeb7
speckledhunger <speckledhunger@users.noreply.github.com>
2017-02-13 11:01:28 +0000

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -3,5 +3,5 @@
   var sign = prompt("Choose to multiply (*), add (+), divide (/) or minus (-)")
   var num1 = parseint(input1)
   var num2 = parseint(input2)
-  var product = num1 sign num2
+  var product = num1 + sign + num2
   alert (num1 + " " + sign + " " + num2 + " " + "=" + " " + product)
