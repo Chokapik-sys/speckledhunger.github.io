832039a2503103457ea7ef4e3c80b8b8402189bc
speckledhunger <speckledhunger@users.noreply.github.com>
2017-06-01 20:33:13 +0100

    Update moves.js

--- chess/moves.js
+++ chess/moves.js
@@ -24,3 +24,53 @@ function 7() {
 function 8() {
   
 }
+//row2
+function 9() {
+  
+}
+function 10() {
+  
+}
+function 11() {
+  
+}
+function 12() {
+  
+}
+function 13() {
+  
+}
+function 14() {
+  
+}
+function 15() {
+  
+}
+function 16() {
+  
+}
+//row3
+function 17() {
+  
+}
+function 18() {
+  
+}
+function 19() {
+  
+}
+function 20() {
+  
+}
+function 21() {
+  
+}
+function 22() {
+  
+}
+function 23() {
+  
+}
+function 24() {
+  
+}
