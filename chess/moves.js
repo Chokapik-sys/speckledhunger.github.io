77783f167b258a39a691aef682a5d71991526363
speckledhunger <speckledhunger@users.noreply.github.com>
2017-06-07 12:54:47 +0100

    Update moves.js

--- chess/moves.js
+++ chess/moves.js
@@ -16,3 +16,34 @@ function coord2(){
   console.log(square)
   move_piece()
 }
+function coord3(){
+  square = "_3_"
+  console.log(square)
+  move_piece()
+}
+function coord4(){
+  square = "_4_"
+  console.log(square)
+  move_piece()
+}
+function coord5(){
+  square = "_5_"
+  console.log(square)
+  move_piece()
+}
+function coord6(){
+  square = "_6_"
+  console.log(square)
+  move_piece()
+}
+function coord7(){
+  square = "_7_"
+  console.log(square)
+  move_piece()
+}
+function coord8(){
+  square = "_8_"
+  console.log(square)
+  move_piece()
+}
+//row2
