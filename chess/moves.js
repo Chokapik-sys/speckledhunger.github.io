e3814025481009261a1dc2c8f6ac964887fa4e8c
speckledhunger <speckledhunger@users.noreply.github.com>
2017-06-07 08:45:49 +0100

    Update moves.js

--- chess/moves.js
+++ chess/moves.js
@@ -1,7 +1,7 @@
 var movecount = 1
 var square = 0
 function move(){
-  if (document.getElementById(square) === ("White Rook")) {
+  if (document.getElementById(square) === "White Rook") {
   console.log("_" + parseint(square) + 1 + "_")
   console.log(square + 2)
   console.log(square + 3)
