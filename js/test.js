04940f164b71d2d5f615a5defd1b10491ee36438
speckledhunger <speckledhunger@users.noreply.github.com>
2017-06-09 12:58:16 +0100

    Create test.js

--- js/test.js
+++ js/test.js
@@ -1,6 +1,7 @@
+function alert() {
 alert("Welcome to the Games site");
 alert("Feel free to browse...");
-
+}
 function randomIframe(obj){
 var scratch = new Array();
 scratch[0] = "https://scratch.mit.edu/projects/embed/89672994/";
