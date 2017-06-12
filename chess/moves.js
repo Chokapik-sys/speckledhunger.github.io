16602cecec51ab0e4acd34287eff9c30de434b10
speckledhunger <speckledhunger@users.noreply.github.com>
2017-06-12 08:40:11 +0100

    Create moves.js

--- chess/moves.js
+++ chess/moves.js
@@ -29,49 +29,65 @@ function phase_change(){
 function coord1(){
   square = "_1_"
   console.log(constant1 + square)
+  if (phase === 0) {
   phase_change()
+  }
   move_piece()
 }
 function coord2(){
   square = "_2_"
   console.log(constant1 + square)
+  if (phase === 0) {
   phase_change()
+  }
   move_piece()
 }
 function coord3(){
   square = "_3_"
   console.log(constant1 + square)
+  if (phase === 0) {
   phase_change()
+  }
   move_piece()
 }
 function coord4(){
   square = "_4_"
   console.log(constant1 + square)
+  if (phase === 0) {
   phase_change()
+  }
   move_piece()
 }
 function coord5(){
   square = "_5_"
   console.log(constant1 + square)
+  if (phase === 0) {
   phase_change()
+  }
   move_piece()
 }
 function coord6(){
   square = "_6_"
   console.log(constant1 + square)
+  if (phase === 0) {
   phase_change()
+  }
   move_piece()
 }
 function coord7(){
   square = "_7_"
   console.log(constant1 + square)
+  if (phase === 0) {
   phase_change()
+  }
   move_piece()
 }
 function coord8(){
   square = "_8_"
   console.log(constant1 + square)
+  if (phase === 0) {
   phase_change()
+  }
   move_piece()
 }
 //row2
