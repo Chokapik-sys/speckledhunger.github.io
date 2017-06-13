a4bd88d59974f59f967b699ccb4e74f13d63b730
speckledhunger <speckledhunger@users.noreply.github.com>
2017-06-13 08:52:32 +0100

    Create gameControllers.js

--- chess/gameControllers.js
+++ chess/gameControllers.js
@@ -1,8 +1,13 @@
+var A = "#BAF7F9"
 function resetTurn() {
   phase = 0
   phase_0()
 }
 function phase_0() {
   if (phase === 0){
-  }
-}
+    for(var i = 0, i = 64, i++) {
+      if (document.getElementById("_" + i + "_").class === "A"){
+    document.getElementById("_" + i + "_").style.backgroundColor = A
+    }
+        }
+        }
