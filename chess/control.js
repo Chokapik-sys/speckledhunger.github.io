673e63e5fd55004d5bb179091da28a1903724820
speckledhunger <speckledhunger@users.noreply.github.com>
2017-06-14 08:48:26 +0100

    Create control.js

--- chess/control.js
+++ chess/control.js
@@ -1,5 +1,6 @@
 var i = 1
 var A = "#BAF7F9"
+var B = "#D0FFB6"
 function resetTurn() {
   phase = 3
   phase_3()
@@ -9,6 +10,9 @@ function phase_3() {
     for (i = 1; i < 65; i++) {
       if (document.getElementById("_" + i + "_").className === "A"){
     document.getElementById("_" + i + "_").style.backgroundColor = A
+      }
+      if (document.getElementById("_" + i + "_").className === "B"){
+    document.getElementById("_" + i + "_").style.backgroundColor = B
       }
     }
   }
