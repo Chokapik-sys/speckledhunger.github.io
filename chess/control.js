4f51820dd8522644363b29f9acbf819fb3513fc6
speckledhunger <speckledhunger@users.noreply.github.com>
2017-06-14 08:48:47 +0100

    Create control.js

--- chess/control.js
+++ chess/control.js
@@ -14,6 +14,7 @@ function phase_3() {
       if (document.getElementById("_" + i + "_").className === "B"){
     document.getElementById("_" + i + "_").style.backgroundColor = B
       }
+      phase = 0
     }
   }
 }
