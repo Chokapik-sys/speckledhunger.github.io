6c4011018dee535fcfe7628888025b2e75b67aab
speckledhunger <speckledhunger@users.noreply.github.com>
2017-06-19 08:36:09 +0100

    Update moves.js

--- chess/moves.js
+++ chess/moves.js
@@ -23,6 +23,14 @@ function move_piece(){
   }
      if (document.getElementById(square).innerHTML === "White Knight") {
        console.log("White Knight in " + square)
+       if (document.getElementById("_" + (parseInt(square.replace("_", "")) - 10) + "_").innerHTML === ""){
+         console.log("White pawn can move to " + "_" + (parseInt(square.replace("_", "")) - 10) + "_")
+         document.getElementById("_" + (parseInt(square.replace("_", "")) - 10) + "_").style.backgroundColor = "#FFF370"
+         piece = "White Pawn"
+         prev_square = square
+         console.log(constant3 + prev_square)
+         phase_change()
+       } 
      }
     //Black Pieces
     if (document.getElementById(square).innerHTML === "Black Pawn") {
