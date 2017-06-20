489d1a76c56d3d9acb7c83604ff5875adf40a98c
speckledhunger <speckledhunger@users.noreply.github.com>
2017-06-20 08:57:29 +0100

    Update moves.js

--- chess/moves.js
+++ chess/moves.js
@@ -22,7 +22,7 @@ function move_piece(){
     }
   }
      if (document.getElementById(square).innerHTML === "White Knight") {
-       if (parseInt(square.replace("_", "")) % 8 == "1") {
+       if (parseInt(square.replace("_", "")) % 8 == "7") {
          console.log("Success")
        } else {
        console.log("White Knight in " + square)
