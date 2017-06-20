923db7bfcfed4f85997525fd68f94afe751297f6
speckledhunger <speckledhunger@users.noreply.github.com>
2017-06-20 08:47:24 +0100

    Update moves.js

--- chess/moves.js
+++ chess/moves.js
@@ -22,9 +22,6 @@ function move_piece(){
     }
   }
      if (document.getElementById(square).innerHTML === "White Knight") {
-       if (parseInt(square.replace("_", "") % 8 == "1") {
-           console.log("Your piece is at the beginning of the board")
-           } else {
        console.log("White Knight in " + square)
        if (document.getElementById("_" + (parseInt(square.replace("_", "")) - 10) + "_").innerHTML === ""){
          console.log("White Knight can move to " + "_" + (parseInt(square.replace("_", "")) - 10) + "_")
@@ -83,7 +80,6 @@ function move_piece(){
          console.log(constant3 + prev_square)
          phase = 2
        }
-     }
      }
     //Black Pieces
     if (document.getElementById(square).innerHTML === "Black Pawn") {
