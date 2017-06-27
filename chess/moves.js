ad5fdce50d0884111434f45115fe27d5d51804df
speckledhunger <speckledhunger@users.noreply.github.com>
2017-06-27 08:14:35 +0100

    Update moves.js

--- chess/moves.js
+++ chess/moves.js
@@ -22,9 +22,12 @@ function move_piece(){
     }
   }
      if (document.getElementById(square).innerHTML === "White Knight") {
-       if ((parseInt(square.replace("_", "")) % 8 == "7" || parseInt(square.replace("_", "")) % 8 == "0") && (parseInt(square.replace("_", "")) % 8 == "7") && (document.getElementById("_" + (parseInt(square.replace("_", "")) + 10) + "_").innerHTML === "")) {
+       if ((parseInt(square.replace("_", "")) % 8 == "7") && (document.getElementById("_" + (parseInt(square.replace("_", "")) + 10) + "_").innerHTML === "")) {
          console.log("Success")
-         document.getElementById("_" + (parseInt(square.replace("_", "")) + 10) + "_").innerHTML = "Yesss"
+         document.getElementById("_" + (parseInt(square.replace("_", "")) + 10) + "_").innerHTML = ""
+       } if ((parseInt(square.replace("_", "")) % 8 == "7") && (document.getElementById("_" + (parseInt(square.replace("_", "")) - 6) + "_").innerHTML === "")) {
+         console.log("Success")
+         document.getElementById("_" + (parseInt(square.replace("_", "")) - 6) + "_").innerHTML = ""
        }
        console.log("White Knight in " + square)
        if (document.getElementById("_" + (parseInt(square.replace("_", "")) - 10) + "_").innerHTML === ""){
