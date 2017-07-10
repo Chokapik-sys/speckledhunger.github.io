475608975729575e803d170a5567c7e7cf5d0a1f
speckledhunger <speckledhunger@users.noreply.github.com>
2017-07-10 21:46:52 +0100

    Update moves.js

--- chess/moves.js
+++ chess/moves.js
@@ -948,7 +948,7 @@ function move_piece(){
   }
     if (document.getElementById(square).innerHTML === "White Bishop") {
         for (mb = 0; mb < 3; mb++)
-          document.getElementById("_" parseInt(square.replace("_", "")) + 9mb + "_").style.backgroundColor = "Red"
+          document.getElementById("_" parseInt(square.replace("_", "")) + (9 * mb) + "_").style.backgroundColor = "Red"
         }  
     //Black Pieces
     if (document.getElementById(square).innerHTML === "Black Pawn") {
