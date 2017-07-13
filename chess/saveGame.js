85d6e6e8eed17019da709fa18863aa0e0d29d7e8
speckledhunger <speckledhunger@users.noreply.github.com>
2017-07-13 21:20:13 +0100

    Update and rename saveGame.js to saveGame.php

--- chess/saveGame.js
+++
@@ -1,4 +0,0 @@
-var expire = new Date()
-function save() {
-  document.cookie = "_1_ =" + document.getElementById("_1_").innerHTML + "; expires=" + (expire.getTime() + 1000 * 36000).toUTCString() + ";"
-}
