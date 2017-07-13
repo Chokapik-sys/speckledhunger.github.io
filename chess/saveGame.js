1b3ffcf8d044e7a655d44f71dac4f93cfb7b5f5e
speckledhunger <speckledhunger@users.noreply.github.com>
2017-07-13 20:21:08 +0100

    Update saveGame.js

--- chess/saveGame.js
+++ chess/saveGame.js
@@ -1,3 +1,4 @@
+var year = new Date()
 function save() {
-  document.cookie = "_1_ =" + document.getElementById("_1_").innerHTML + "; expires= 365;"
+  document.cookie = "_1_ =" + document.getElementById("_1_").innerHTML + "; expires=" + (year.getFullYear() + 1) + ";"
 }
