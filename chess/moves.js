e49982f40688207c3793c0076dec7aa522684f14
speckledhunger <speckledhunger@users.noreply.github.com>
2017-07-15 15:08:24 +0100

    Update moves.js

--- chess/moves.js
+++ chess/moves.js
@@ -22,8 +22,6 @@ var constant3 = "Moving from:"
 function move_piece(){
   //phase1
   if (phase === 1) {
-    //Take Pieces (rules.js)
-    phase_1()
     //White Pieces
     if (document.getElementById(square).innerHTML === "White Pawn") {
       if (document.getElementById("_" + (parseInt(square.replace("_", "")) + 8) + "_").innerHTML === ""){
@@ -315,16 +313,20 @@ function move_piece(){
   }
   //phase2
   if (phase === 2) {
+    phase_2()
+  }
+  //phase3
+  if (phase === 3) {
     if (document.getElementById(square).style.backgroundColor == "rgb(255, 243, 112)") {
       document.getElementById(prev_square).innerHTML = ""
       document.getElementById(square).innerHTML = piece
       phase_change()
-      phase_3()
+      phase_4()
     }
   }
 }
 function phase_change(){
-  if (phase === 3) {
+  if (phase === 4) {
       phase = 0
       } else {
         phase = phase + 1
