58db41485b20a328085a09cfeeb9739948f26720
speckledhunger <speckledhunger@users.noreply.github.com>
2017-07-15 23:18:08 +0100

    Update rules.js

--- chess/rules.js
+++ chess/rules.js
@@ -1,11 +1,14 @@
+var capture = 0
 function phase_2() {
   if (phase === 2) {
   if (piece === "White Pawn") {
     if  (document.getElementById("_" + (parseInt(square.replace("_", "")) + 9) + "_").innerHTML[0] === "B" && (parseInt(square.replace("_", "")) + 9) % 8 != "0"){
       document.getElementById("_" + (parseInt(square.replace("_", "")) + 9) + "_").style.backgroundColor = "#FF8466"
+      capture = 1
     }
-  }
+  } if (capture === 1 || pawnmove === 1 || knightmove === 1 || bishopmove === 1 || rookmove === 1 || queenmove === 1) {
   phase = 3
   move_piece()
+  }
   }
 }
