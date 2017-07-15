6e28f2c36b9bfa187d32d85d6053b39e12ade290
speckledhunger <speckledhunger@users.noreply.github.com>
2017-07-15 14:39:17 +0100

    Update rules.js

--- chess/rules.js
+++ chess/rules.js
@@ -1,7 +1,8 @@
 function phase_2() {
   if (piece === "White Pawn") {
-    document.getElementById("_" + (parseInt(square.replace("_", "")) + 9) + "_").style.backgroundColor = "#FF8466"
-    document.getElementById("_" + (parseInt(square.replace("_", "")) + 7) + "_").style.backgroundColor = "#FF8466"
+    if  (document.getElementById("_" + (parseInt(square.replace("_", "")) + 9) + "_").innerHTML !== "" || (parseInt(square.replace("_", "")) + 9)) % 8 != "0"){
+      document.getElementById("_" + (parseInt(square.replace("_", "")) + 9) + "_").style.backgroundColor = "#FF8466"
+    }
   }
   phase = 3
   move_piece()
