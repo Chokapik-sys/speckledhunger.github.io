6f48f8995a31bd4559fc2c47cd296e55d096c6e5
speckledhunger <speckledhunger@users.noreply.github.com>
2017-07-16 22:55:32 +0100

    Update rules.js

--- chess/rules.js
+++ chess/rules.js
@@ -6,17 +6,41 @@ function phase_2() {
     if  (document.getElementById("_" + (parseInt(square.replace("_", "")) + 9) + "_").innerHTML === "Black King" && (parseInt(square.replace("_", "")) + 9) % 8 != "1"){
       blackcheck = 1
       console.log("Black King in check")
-    }
-    else if  (document.getElementById("_" + (parseInt(square.replace("_", "")) + 9) + "_").innerHTML[0] === "B" && (parseInt(square.replace("_", "")) + 9) % 8 != "1"){
+    } else if (document.getElementById("_" + (parseInt(square.replace("_", "")) + 9) + "_").innerHTML[0] === "B" && (parseInt(square.replace("_", "")) + 9) % 8 != "1"){
       document.getElementById("_" + (parseInt(square.replace("_", "")) + 9) + "_").style.backgroundColor = "#FF8466"
       capture = 1
-    }
-    if  (document.getElementById("_" + (parseInt(square.replace("_", "")) + 7) + "_").innerHTML === "Black King" && (parseInt(square.replace("_", "")) + 7) % 8 != "0"){
+    } if (document.getElementById("_" + (parseInt(square.replace("_", "")) + 7) + "_").innerHTML === "Black King" && (parseInt(square.replace("_", "")) + 7) % 8 != "0"){
       blackcheck = 1
       console.log("Black King in check")
-    }
-    else if  (document.getElementById("_" + (parseInt(square.replace("_", "")) + 7) + "_").innerHTML[0] === "B" && (parseInt(square.replace("_", "")) + 7) % 8 != "0"){
+    } else if (document.getElementById("_" + (parseInt(square.replace("_", "")) + 7) + "_").innerHTML[0] === "B" && (parseInt(square.replace("_", "")) + 7) % 8 != "0"){
       document.getElementById("_" + (parseInt(square.replace("_", "")) + 7) + "_").style.backgroundColor = "#FF8466"
+      capture = 1
+    }
+  }
+    if (piece === "White Rook") {
+    if (document.getElementById("_" + (parseInt(square.replace("_", "")) mr1 + 1) + "_").innerHTML === "Black King" && (parseInt(square.replace("_", "")) + mr1 + 1) % 8 != "1"){
+      blackcheck = 1
+      console.log("Black King in check")
+    } else if (document.getElementById("_" + (parseInt(square.replace("_", "")) mr1 + 1) + "_").innerHTML[0] === "B" && (parseInt(square.replace("_", "")) + mr1 + 1) % 8 != "1"){
+      document.getElementById("_" + (parseInt(square.replace("_", "")) + mr1 + 1) + "_").style.backgroundColor = "#FF8466"
+      capture = 1
+    } if (document.getElementById("_" + (parseInt(square.replace("_", "")) + mr2 - 1) + "_").innerHTML === "Black King" && (parseInt(square.replace("_", "")) + mr2 - 1) % 8 != "0"){
+      blackcheck = 1
+      console.log("Black King in check")
+    } else if (document.getElementById("_" + (parseInt(square.replace("_", "")) + mr2 - 1) + "_").innerHTML[0] === "B" && (parseInt(square.replace("_", "")) + mr2 - 1) % 8 != "0"){
+      document.getElementById("_" + (parseInt(square.replace("_", "")) + mr2 - 1) + "_").style.backgroundColor = "#FF8466"
+      capture = 1
+    } if (document.getElementById("_" + (parseInt(square.replace("_", "")) mr3 + 1) + "_").innerHTML === "Black King"){
+      blackcheck = 1
+      console.log("Black King in check")
+    } else if (document.getElementById("_" + (parseInt(square.replace("_", "")) mr3 + 8) + "_").innerHTML[0] === "B"){
+      document.getElementById("_" + (parseInt(square.replace("_", "")) + mr3 + 8) + "_").style.backgroundColor = "#FF8466"
+      capture = 1
+    } if (document.getElementById("_" + (parseInt(square.replace("_", "")) + mr4 - 8) + "_").innerHTML === "Black King"){
+      blackcheck = 1
+      console.log("Black King in check")
+    } else if (document.getElementById("_" + (parseInt(square.replace("_", "")) + mr4 - 8) + "_").innerHTML[0] === "B"){
+      document.getElementById("_" + (parseInt(square.replace("_", "")) + mr4 - 8) + "_").style.backgroundColor = "#FF8466"
       capture = 1
     }
   }
