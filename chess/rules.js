9b672cdbddd94fa1d4da9d09f072295046b1d642
speckledhunger <speckledhunger@users.noreply.github.com>
2017-07-16 22:40:34 +0100

    Update rules.js

--- chess/rules.js
+++ chess/rules.js
@@ -3,11 +3,11 @@ var balckcheck = 0
 function phase_2() {
   if (phase === 2) {
   if (piece === "White Pawn") {
-    if  (document.getElementById("_" + (parseInt(square.replace("_", "")) + 9) + "_").innerHTML === "Black King" && (parseInt(square.replace("_", "")) + 9) % 8 != "0"){
+    if  (document.getElementById("_" + (parseInt(square.replace("_", "")) + 9) + "_").innerHTML === "Black King" && (parseInt(square.replace("_", "")) + 9) % 8 != "1"){
       blackcheck = 1
       console.log("Black King in check")
     }
-    else if  (document.getElementById("_" + (parseInt(square.replace("_", "")) + 9) + "_").innerHTML[0] === "B" && (parseInt(square.replace("_", "")) + 9) % 8 != "0"){
+    else if  (document.getElementById("_" + (parseInt(square.replace("_", "")) + 9) + "_").innerHTML[0] === "B" && (parseInt(square.replace("_", "")) + 9) % 8 != "1"){
       document.getElementById("_" + (parseInt(square.replace("_", "")) + 9) + "_").style.backgroundColor = "#FF8466"
       capture = 1
     }
