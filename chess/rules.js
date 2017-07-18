71152c6c800263e5abab69f2dcc7350826d39fc0
speckledhunger <speckledhunger@users.noreply.github.com>
2017-07-18 18:09:26 +0100

    Update rules.js

--- chess/rules.js
+++ chess/rules.js
@@ -24,6 +24,12 @@ function phase_2() {
     } else if (document.getElementById("_" + (parseInt(square.replace("_", "")) - 10) + "_").innerHTML[0] === "B" && (parseInt(square.replace("_", "")) - 10) % 8 != "1" && (parseInt(square.replace("_", "")) - 10) % 8 != "2") {
       blackcheck = 1
       console.log("Black King in check")
+    } if (document.getElementById("_" + (parseInt(square.replace("_", "")) + 10) + "_").innerHTML === "Black King" && (parseInt(square.replace("_", "")) + 10) % 8 != "1" && (parseInt(square.replace("_", "")) - 10) % 8 != "2") {
+      blackcheck = 1
+      console.log("Black King in check")
+    } else if (document.getElementById("_" + (parseInt(square.replace("_", "")) + 10) + "_").innerHTML[0] === "B" && (parseInt(square.replace("_", "")) + 10) % 8 != "1" && (parseInt(square.replace("_", "")) - 10) % 8 != "2") {
+      blackcheck = 1
+      console.log("Black King in check")
     }
     }
     if (piece === "White Rook") {
