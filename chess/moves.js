e14423fb3c7c8a7a33d893f4d28b1b20918129c1
speckledhunger <speckledhunger@users.noreply.github.com>
2017-08-04 22:47:59 +0100

    Update moves.js

--- chess/moves.js
+++ chess/moves.js
@@ -150,6 +150,27 @@ function move_piece(){
       if (document.getElementById("_" + (parseInt(square.replace("_", "")) + 8) + "_").innerHTML === "" && S_whitecheck == "0") {
         document.getElementById("_" + (parseInt(square.replace("_", "")) + 8) + "_").style.backgroundColor = "#FFF370"
         kingmove = 1
+      } if (document.getElementById("_" + (parseInt(square.replace("_", "")) - 8) + "_").innerHTML === "" && N_whitecheck == "0") {
+        document.getElementById("_" + (parseInt(square.replace("_", "")) - 8) + "_").style.backgroundColor = "#FFF370"
+        kingmove = 1
+      } if (document.getElementById("_" + (parseInt(square.replace("_", "")) + 1) + "_").innerHTML === "" && E_whitecheck == "0") {
+        document.getElementById("_" + (parseInt(square.replace("_", "")) + 1) + "_").style.backgroundColor = "#FFF370"
+        kingmove = 1
+      } if (document.getElementById("_" + (parseInt(square.replace("_", "")) - 1) + "_").innerHTML === "" && W_whitecheck == "0") {
+        document.getElementById("_" + (parseInt(square.replace("_", "")) - 1) + "_").style.backgroundColor = "#FFF370"
+        kingmove = 1
+      } if (document.getElementById("_" + (parseInt(square.replace("_", "")) + 7) + "_").innerHTML === "" && SW_whitecheck == "0") {
+        document.getElementById("_" + (parseInt(square.replace("_", "")) + 7) + "_").style.backgroundColor = "#FFF370"
+        kingmove = 1
+      } if (document.getElementById("_" + (parseInt(square.replace("_", "")) - 7) + "_").innerHTML === "" && NE_whitecheck == "0") {
+        document.getElementById("_" + (parseInt(square.replace("_", "")) - 7) + "_").style.backgroundColor = "#FFF370"
+        kingmove = 1
+      } if (document.getElementById("_" + (parseInt(square.replace("_", "")) + 9) + "_").innerHTML === "" && SE_whitecheck == "0") {
+        document.getElementById("_" + (parseInt(square.replace("_", "")) + 9) + "_").style.backgroundColor = "#FFF370"
+        kingmove = 1
+      } if (document.getElementById("_" + (parseInt(square.replace("_", "")) - 9) + "_").innerHTML === "" && NW_whitecheck == "0") {
+        document.getElementById("_" + (parseInt(square.replace("_", "")) - 9) + "_").style.backgroundColor = "#FFF370"
+        kingmove = 1
       }
       if (kingmove == 1) {
         prev_square = square
