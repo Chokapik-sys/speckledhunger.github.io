92557994018af02ef67699bd640cc4bc3794ed17
speckledhunger <speckledhunger@users.noreply.github.com>
2017-08-10 16:16:03 +0100

    Update topRow.js

--- js/topRow.js
+++ js/topRow.js
@@ -5,4 +5,7 @@ while (scrsize != window.innerWidth) {
 function updatesize() {
   document.getElementById("box").style.left =  0.5 * (window.innerWidth - document.getElementById("title").style.width) + "px"
   scrsize = window.innerWidth
+  while (scrsize != window.innerWidth) {
+    updatesize()
+  }
 }
