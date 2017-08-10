a96d272e9a4654af807ce1733104b17242814270
speckledhunger <speckledhunger@users.noreply.github.com>
2017-08-10 16:59:08 +0100

    Update topRow.js

--- js/topRow.js
+++ js/topRow.js
@@ -1,5 +1,5 @@
 var update = setInterval(updatesize, 100)
 function updatesize() {
-  document.getElementById("box").style.width = document.getElementById("title").offsetWidth
+  document.getElementById("box").style.width = document.getElementById("title").offsetWidth + "px"
   document.getElementById("box").style.left =  0.5 * (window.innerWidth - (document.getElementById("box").style.width - "px")) + "px"
 }
