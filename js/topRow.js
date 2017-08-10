dcf074a4db1192b45304668797ad7a3294a667dc
speckledhunger <speckledhunger@users.noreply.github.com>
2017-08-10 15:57:31 +0100

    Update topRow.js

--- js/topRow.js
+++ js/topRow.js
@@ -1,4 +1,5 @@
-var starttime = new Date()
-for (var endtime = new Date(); endtime.getMilliseconds() > (starttime.getMilliseconds() + 50;)) {
-document.getElementById("box").style.left =  (0.5 * window.innerWidth) - document.getElementById("title").style.width + "px"
+var scrsize = window.innerWidth
+if (scrsize != window.innerWidth) {
+  document.getElementById("box").style.left =  (0.5 * window.innerWidth) - document.getElementById("title").style.width + "px"
+  scrsize = window.innerWidth
 }
