3a3ef4a19e953da0511bd93db84c63feda87517f
speckledhunger <speckledhunger@users.noreply.github.com>
2017-08-10 15:37:14 +0100

    Update topRow.js

--- js/topRow.js
+++ js/topRow.js
@@ -1,1 +1,1 @@
-document.getElementById("box").style.left =  (0.5 * screen.width) - document.getElementById("title").style.width + "px"
+document.getElementById("box").style.left =  (0.5 * window.innerWidth) - document.getElementById("title").style.width + "px"
