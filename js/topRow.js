fbda9d23ebb572a22e1a865bc7914d86fd6c4b66
speckledhunger <speckledhunger@users.noreply.github.com>
2017-08-10 15:49:26 +0100

    Update topRow.js

--- js/topRow.js
+++ js/topRow.js
@@ -1,4 +1,4 @@
-var time = new date()
-while (date() > time) {
+var starttime = new Date()
+for (var endtime = new Date(); endtime.getMilliseconds() > (starttime.getMilliseconds() + 50;)) {
 document.getElementById("box").style.left =  (0.5 * window.innerWidth) - document.getElementById("title").style.width + "px"
 }
