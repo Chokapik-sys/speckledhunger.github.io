9ab58685330b9dc51bbcfb1115e3ca5b876ec00a
speckledhunger <speckledhunger@users.noreply.github.com>
2017-08-12 18:36:39 +0100

    Update topRow.js

--- js/topRow.js
+++ js/topRow.js
@@ -1,6 +1,6 @@
 var update = setInterval(updatesize, 1)
-document.getElementById("title").style.fontheight = "29px"
 function updatesize() {
+  document.getElementById("title").style.fontheight = "29px"
   document.getElementById("box").style.width = document.getElementById("title").offsetWidth + "px"
   document.getElementById("box").style.left =  0.5 * (window.innerWidth - (document.getElementById("box").style.width.replace("px", ""))) + "px"
   document.getElementById("box").style.height = window.innerHeight * (60/678) + "px";document.getElementById("topRow").style.height = window.innerHeight * (60/678) + "px"
