3eec51c378e96aa671e171681c9a1a48a02ae805
speckledhunger <speckledhunger@users.noreply.github.com>
2017-08-12 18:25:08 +0100

    Update topRow.js

--- js/topRow.js
+++ js/topRow.js
@@ -1,14 +1,15 @@
 var update = setInterval(updatesize, 1)
-var fonth = 29
+var fontheight = 29
 function updatesize() {
   document.getElementById("box").style.width = document.getElementById("title").offsetWidth + "px"
   document.getElementById("box").style.left =  0.5 * (window.innerWidth - (document.getElementById("box").style.width.replace("px", ""))) + "px"
   document.getElementById("box").style.height = window.innerHeight * (60/678) + "px";document.getElementById("topRow").style.height = window.innerHeight * (60/678) + "px"
   if (document.getElementById("title").offsetWidth / parseInt(document.getElementById("title").style.fontSize) != 11.551724137931034) {
     if (document.getElementById("title").offsetWidth / parseInt(document.getElementById("title").style.fontSize) > 11.551724137931034) {
-      document.getElementById("title").fontsize += 1
+      fontheight += 1
     } if (document.getElementById("title").offsetWidth / parseInt(document.getElementById("title").style.fontSize) < 11.551724137931034) {
-      document.getElementById("title").fontsize -= 1
+      fontheight -= 1
       }
   }
+  document.getElementById("title").style.fontSize = fontheight + "px"
 }
