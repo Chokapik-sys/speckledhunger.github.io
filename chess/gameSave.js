51fca178acc99288e4b3324dc371a0978631e807
speckledhunger <speckledhunger@users.noreply.github.com>
2017-08-15 14:12:52 +0100

    Update gameSave.js

--- chess/gameSave.js
+++ chess/gameSave.js
@@ -6,7 +6,6 @@ var WKNcur = 0
 var WROcur = 0
 var WPAcur = 0
 var WQUcur = 0
-var store = document.cookie
 var date = new Date()
 function save() {
   date = new Date()
@@ -14,21 +13,21 @@ function save() {
   for (var i = 1; i < 65; i++) {
     if (document.getElementById("_" + i + "_").innerHTML === "Black Pawn") {
       if (BPAcur === 0) {
-        store("BPA1=" + "_" + i + "_; expires=" + (date.toUTCString))
+        document.cookie = ("BPA1=" + "_" + i + "_; expires=" + (date.toUTCString))
       } else if (BPAcur === 1) {
-        store("BPA2=" + "_" + i + "_; expires=" + (date.toUTCString))
+        document.cookie = ("BPA2=" + "_" + i + "_; expires=" + (date.toUTCString))
       } else if (BPAcur === 2) {
-        store("BPA3=" + "_" + i + "_; expires=" + (date.toUTCString))
+        document.cookie = ("BPA3=" + "_" + i + "_; expires=" + (date.toUTCString))
       } else if (BPAcur === 3) {
-        store("BPA4=" + "_" + i + "_; expires=" + (date.toUTCString))
+        document.cookie = ("BPA4=" + "_" + i + "_; expires=" + (date.toUTCString))
       } else if (BPAcur === 4) {
-        store("BPA5=" + "_" + i + "_; expires=" + (date.toUTCString))
+        document.cookie = ("BPA5=" + "_" + i + "_; expires=" + (date.toUTCString))
       } else if (BPAcur === 5) {
-        store("BPA6=" + "_" + i + "_; expires=" + (date.toUTCString))
+        document.cookie = ("BPA6=" + "_" + i + "_; expires=" + (date.toUTCString))
       } else if (BPAcur === 6) {
-        store("BPA7=" + "_" + i + "_; expires=" + (date.toUTCString))
+        document.cookie = ("BPA7=" + "_" + i + "_; expires=" + (date.toUTCString))
       } else if (BPAcur === 7) {
-        store("BPA8=" + "_" + i + "_; expires=" + (date.toUTCString))
+        document.cookie = ("BPA8=" + "_" + i + "_; expires=" + (date.toUTCString))
       } BPAcur ++
     }
   }
