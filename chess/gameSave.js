873d77ef14fd1d5727462643d56bcec4df824f14
speckledhunger <speckledhunger@users.noreply.github.com>
2017-08-15 14:48:53 +0100

    Update gameSave.js

--- chess/gameSave.js
+++ chess/gameSave.js
@@ -7,27 +7,28 @@ var WROcur = 0
 var WPAcur = 0
 var WQUcur = 0
 var i = 1
-var date = new Date()
+var expire = 0
 function save() {
-  date.setFullYear(date.getUTCFullYear + 1)
+  expire = new Date()
+  expire.setFullYear(expire.getUTCFullYear + 1)
   for (i = 1; i < 65; i++) {
     if (document.getElementById("_" + i + "_").innerHTML === "Black Pawn") {
       if (BPAcur === 0) {
-        document.cookie = ("BPA1=" + "_" + i + "_; expires=" + date.toUTCString())
+        document.cookie = ("BPA1=" + "_" + i + "_; expires=" + expire.toUTCString())
       } else if (BPAcur === 1) {
-        document.cookie = ("BPA2=" + "_" + i + "_; expires=" + date.toUTCString())
+        document.cookie = ("BPA2=" + "_" + i + "_; expires=" + expire.toUTCString())
       } else if (BPAcur === 2) {
-        document.cookie = ("BPA3=" + "_" + i + "_; expires=" + date.toUTCString())
+        document.cookie = ("BPA3=" + "_" + i + "_; expires=" + expire.toUTCString())
       } else if (BPAcur === 3) {
-        document.cookie = ("BPA4=" + "_" + i + "_; expires=" + date.toUTCString())
+        document.cookie = ("BPA4=" + "_" + i + "_; expires=" + expire.toUTCString())
       } else if (BPAcur === 4) {
-        document.cookie = ("BPA5=" + "_" + i + "_; expires=" + date.toUTCString())
+        document.cookie = ("BPA5=" + "_" + i + "_; expires=" + expire.toUTCString())
       } else if (BPAcur === 5) {
-        document.cookie = ("BPA6=" + "_" + i + "_; expires=" + date.toUTCString())
+        document.cookie = ("BPA6=" + "_" + i + "_; expires=" + expire.toUTCString())
       } else if (BPAcur === 6) {
-        document.cookie = ("BPA7=" + "_" + i + "_; expires=" + date.toUTCString())
+        document.cookie = ("BPA7=" + "_" + i + "_; expires=" + expire.toUTCString())
       } else if (BPAcur === 7) {
-        document.cookie = ("BPA8=" + "_" + i + "_; expires=" + date.toUTCString())
+        document.cookie = ("BPA8=" + "_" + i + "_; expires=" + expire.toUTCString())
       } BPAcur ++
     }
   }
