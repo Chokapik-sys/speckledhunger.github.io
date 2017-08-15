76b948bd2fc1aa9feaab9686bd6f713db4428ff6
speckledhunger <speckledhunger@users.noreply.github.com>
2017-08-15 14:22:26 +0100

    Update gameSave.js

--- chess/gameSave.js
+++ chess/gameSave.js
@@ -13,21 +13,21 @@ function save() {
   for (var i = 1; i < 65; i++) {
     if (document.getElementById("_" + i + "_").innerHTML === "Black Pawn") {
       if (BPAcur === 0) {
-        document.cookie = ("BPA1=" + "_" + i + "_; expires=" + (date.toUTCString))
+        document.cookie = ("BPA1=" + "_" + i + "_; expires=" + (date.toUTCString()))
       } else if (BPAcur === 1) {
-        document.cookie = ("BPA2=" + "_" + i + "_; expires=" + (date.toUTCString))
+        document.cookie = ("BPA2=" + "_" + i + "_; expires=" + (date.toUTCString()))
       } else if (BPAcur === 2) {
-        document.cookie = ("BPA3=" + "_" + i + "_; expires=" + (date.toUTCString))
+        document.cookie = ("BPA3=" + "_" + i + "_; expires=" + (date.toUTCString()))
       } else if (BPAcur === 3) {
-        document.cookie = ("BPA4=" + "_" + i + "_; expires=" + (date.toUTCString))
+        document.cookie = ("BPA4=" + "_" + i + "_; expires=" + (date.toUTCString()))
       } else if (BPAcur === 4) {
-        document.cookie = ("BPA5=" + "_" + i + "_; expires=" + (date.toUTCString))
+        document.cookie = ("BPA5=" + "_" + i + "_; expires=" + (date.toUTCString()))
       } else if (BPAcur === 5) {
-        document.cookie = ("BPA6=" + "_" + i + "_; expires=" + (date.toUTCString))
+        document.cookie = ("BPA6=" + "_" + i + "_; expires=" + (date.toUTCString()))
       } else if (BPAcur === 6) {
-        document.cookie = ("BPA7=" + "_" + i + "_; expires=" + (date.toUTCString))
+        document.cookie = ("BPA7=" + "_" + i + "_; expires=" + (date.toUTCString()))
       } else if (BPAcur === 7) {
-        document.cookie = ("BPA8=" + "_" + i + "_; expires=" + (date.toUTCString))
+        document.cookie = ("BPA8=" + "_" + i + "_; expires=" + (date.toUTCString()))
       } BPAcur ++
     }
   }
