18a54c955b3fa64ba33a70ac0bdcffaefe710ac2
speckledhunger <speckledhunger@users.noreply.github.com>
2017-09-04 19:31:44 +0100

    Update control.js

--- chess/control.js
+++ chess/control.js
@@ -2,7 +2,8 @@ var cc = 1
 var cb = 1
 var A = "#BAF7F9"
 var B = "#D0FFB6"
-var char = ""
+var charnum = ""
+var charcoord = ""
 function resetTurn() {
   phase = 4
   phase_4()
@@ -39,8 +40,11 @@ function showOption() {
   }
 }
 function checkkey() {
-  char = document.getElementById("num").value[document.getElementById("num").value.length - 1]
-  if (char !== "0" && char !== "1" && char !== "2" && char !== "3" && char !== "4" && char !== "5" && char !== "6" && char !== "7" && char !== "8" && char !== "9") {
-      document.getElementById("num").value = document.getElementById("num").value.replace(char, "")
+  charnum = document.getElementById("num").value[document.getElementById("num").value.length - 1]
+  charcoord = document.getElementById("coord").value[document.getElementById("num").value.length - 1]
+  if (charnum !== "0" && charnum !== "1" && charnum !== "2" && charnum !== "3" && charnum !== "4" && charnum !== "5" && charnum !== "6" && charnum !== "7" && charnum !== "8" && charnum !== "9") {
+      document.getElementById("num").value = document.getElementById("num").value.replace(charnum, "")
+  } if (charcoord !== "_" && charcoord !== "1" && charcoord !== "2" && charcoord !== "3" && charcoord !== "4" && charcoord !== "5" && charcoord !== "6" && charcoord !== "7" && charcoord !== "8" && charcoord !== "9") {
+      document.getElementById("coord").value = document.getElementById("coord").value.replace(charcoord, "")
   }
 }
