97e1272d5f2fab16deb64055dcba5e9b6096d6bb
speckledhunger <speckledhunger@users.noreply.github.com>
2017-12-08 08:48:51 +0000

    Update rules.js

--- chess/rules.js
+++ chess/rules.js
@@ -999,7 +999,7 @@ function aftercheck() {
       document.getElementById("_" + square + "_").innerHTML = prev_inner
       alert("King cannot be moved into check")
       changecolour()
-    } if (blackcheckpattern !== "00000000") {
+    } if (blackcheckpattern !== "00000000" && NW_blackcheck.toString() + N_blackcheck.toString() + NE_blackcheck.toString() + W_blackcheck.toString() + E_blackcheck.toString() + SW_blackcheck.toString() + S_blackcheck.toString() + SE_blackcheck.toString() && NW_blackcheck.toString() + N_blackcheck.toString() + NE_blackcheck.toString() + W_blackcheck.toString() + E_blackcheck.toString() + SW_blackcheck.toString() + S_blackcheck.toString() + SE_blackcheck.toString() !== "00000000") {
       document.getElementById("_" + prev_square + "_").innerHTML = piece
       document.getElementById("_" + square + "_").innerHTML = prev_inner
       alert("King must be moved out of check")
@@ -1011,7 +1011,7 @@ function aftercheck() {
       document.getElementById("_" + square + "_").innerHTML = prev_inner
       alert("King cannot be moved into check")
       changecolour()
-    } if (whitecheckpattern !== "00000000") {
+    } if (whitecheckpattern !== "00000000" && NW_whitecheck.toString() + N_whitecheck.toString() + NE_whitecheck.toString() + W_whitecheck.toString() + E_whitecheck.toString() + SW_whitecheck.toString() + S_whitecheck.toString() + SE_whitecheck.toString() && NW_whitecheck.toString() + N_whitecheck.toString() + NE_whitecheck.toString() + W_whitecheck.toString() + E_whitecheck.toString() + SW_whitecheck.toString() + S_whitecheck.toString() + SE_whitecheck.toString() !== "00000000"") {
       document.getElementById("_" + prev_square + "_").innerHTML = piece
       document.getElementById("_" + square + "_").innerHTML = prev_inner
       alert("King must be moved out of check")
