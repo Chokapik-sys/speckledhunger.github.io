29a52d249a85c050daa33ebee5a4ca5f6eaa0834
speckledhunger <speckledhunger@users.noreply.github.com>
2018-10-17 10:53:30 +0100

    Removed faulty and unneeded 'if' statement

--- chess/gameSave.js
+++ chess/gameSave.js
@@ -332,7 +332,6 @@ function load() {
 function clearsave() {
   var curCookie = document.cookie.split("; ");var result = [];var repeat = 0;var pos; var cursearch; var ca = 0; var arr = 0; var search;
  //From Array_Search in JS-Tools repo
-  if (True) {
     search = cnumset
     for (ca = 0; ca < curCookie.length ; ca ++) {
       repeat = 0
@@ -344,7 +343,7 @@ function clearsave() {
         if (search === cursearch.toLocaleLowerCase()) {
           result.push(curCookie[ca])
           repeat = 1
-        }}}}
+        }}}
   //
   for (var l=0;l<result.length;l++) {
    document.cookie = result[l] +"; expires=Thu, 01 Jan 1970 00:00:00 UTC" 
