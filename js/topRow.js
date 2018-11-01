b39cdd61d5621b47280784246d7122f8b00365b5
speckledhunger <speckledhunger@users.noreply.github.com>
2018-11-01 15:00:23 +0000

    Update topRow.js

--- js/topRow.js
+++ js/topRow.js
@@ -3,4 +3,6 @@ function updatesize() {
   document.getElementById("box").style.width = document.getElementById("title").offsetWidth + "px"
   document.getElementById("box").style.left =  0.5 * (window.innerWidth - (document.getElementById("box").style.width.replace("px", ""))) + "px"
   document.getElementById("box").style.height = window.innerHeight * (60/678) + "px";document.getElementById("topRow").style.height = window.innerHeight * (60/678) + "px"
+  document.getElementById('navmenu').style.top = parseFloat(document.getElementById("box").style.height) + 23 + "px"
 }
+updatesize()
